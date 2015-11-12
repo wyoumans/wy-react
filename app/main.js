fff2b78883c5e25284ea39754118c3b9efe41ce0
William Youmans <will@williamyoumans.com>
2015-11-11 23:03:43 -0500

    adding react, jsx, and babel

--- app/main.js
+++ app/main.js
@@ -1,4 +1,8 @@
-'use strict';
-var component = require('./component.js');
+import React from 'react';
+import Hello from './component.jsx';
 
-document.body.appendChild(component());
+main();
+
+function main() {
+  React.render(<Hello />, document.getElementById('app'));
+}
